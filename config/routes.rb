d7325e5ac43ef11175c69edd874b2b0248fb3bf4
Grimhamr <rossiadisj@gmail.com>
2021-09-03 10:16:20 +0300

    add name to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :comments
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
@@ -9,5 +11,4 @@ Rails.application.routes.draw do
 
   
     root "users#index"
-
 end
