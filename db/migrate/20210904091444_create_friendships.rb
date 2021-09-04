3c793b6261cd2eb981d4453659407bdd44929a8c
Grimhamr <rossiadisj@gmail.com>
2021-09-04 12:15:46 +0300

    edit friendship model

--- db/migrate/20210904091444_create_friendships.rb
+++ db/migrate/20210904091444_create_friendships.rb
@@ -2,12 +2,9 @@ class CreateFriendships < ActiveRecord::Migration[6.1]
   def change
     create_table :friendships do |t|
       t.references :user, null: false, foreign_key: true
-     # t.references :friend, null: false, foreign_key: true
+      t.references :friend, null: false, foreign_key: true
 
       t.timestamps
     end
-
-    add_column :friendships, :friend_id, :integer
-    add_foreign_key :friendships, :users, column: :friend_id
   end
 end
