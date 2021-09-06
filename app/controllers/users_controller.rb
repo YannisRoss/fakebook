07a1c575c1f5c1bfad86a87c4985f8756c49defc
Grimhamr <rossiadisj@gmail.com>
2021-09-06 13:50:04 +0300

    add edit profile page, sanitize name param

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,10 +41,29 @@ class UsersController < ApplicationController
 
     end
 
+    def edit
+        @user=current_user
+    end
+
+    def update
+        if @user.update(user_params)
+            format.html { redirect_to @user, notice: "user was successfully updated." }
+            format.json { render :show, status: :ok, location: @user }
+          else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
+    end
+
     def redirect_unregistered
         unless current_user
             
             redirect_to new_user_registration_url
         end
     end
+    private
+    def user_params
+        params.require(:user).permit(:email, :name)
+      end
+
 end
