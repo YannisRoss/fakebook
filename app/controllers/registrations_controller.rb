2f0363bd8a4418582c531fdf9ef8a31a1e7e4e86
Grimhamr <rossiadisj@gmail.com>
2021-09-08 12:26:23 +0300

    Revert "set up sendgrid mailer for signup email"
    This reverts commit 59d7801ae97f8e8237e979df04eb65a3e04b86ea.

--- app/controllers/registrations_controller.rb
+++
@@ -1,8 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-
-    def create
-      super
-      UserNotifierMailer.send_signup_email(current_user).deliver unless current_user.invalid?
-    end
-  
-  end
